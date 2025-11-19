642a8cdd4ffb322d4eb990a78d60067d561ed5f0
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2025-11-19 10:05:54 -0500

    Deteremined positions for preset stone slots

--- ShiftStoneRandomizer/LoadoutInteractor.cs
+++ ShiftStoneRandomizer/LoadoutInteractor.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem.Utilities;
 using RumbleModdingAPI;
 using System.Collections.Generic;
 using System.Linq;
+using RumbleModdingAPI;
 
 namespace ShiftStoneRandomizer
 {
@@ -37,6 +38,7 @@ namespace ShiftStoneRandomizer
 		public static GameObject ClusterSource;
 		static LoadoutInteractor()
 		{
+			ButtonSource = GameObject.Instantiate(Calls.GameObjects.Gym.LOGIC.Heinhouserproducts.Telephone20REDUXspecialedition.FriendScreen.FriendScrollBar.PageDownButton.GetGameObject());
 			ButtonSource.transform.GetChild(0).GetComponent<InteractionButton>().enabled = true;
 			ButtonSource.transform.localRotation = Quaternion.Euler(0f, 270f, 90f);
 			ButtonSource.transform.localPosition = Vector3.zero;
