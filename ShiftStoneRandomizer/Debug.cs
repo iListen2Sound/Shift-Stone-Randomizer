50b885b66c9aeabe28a5cb0fb80100d20c8983d5
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2025-12-02 10:15:12 -0500

    Whoopsie. Really need to learn how squashing works

--- ShiftStoneRandomizer/Debug.cs
+++ ShiftStoneRandomizer/Debug.cs
@@ -43,31 +43,6 @@ namespace ShiftStoneRandomizer
 			DebugUi.transform.localPosition = new Vector3(0f, 0.1f, 0.96f);
 			DebugUi.transform.SetParent(PlayerUi.transform, false);
 			DebugUiText = DebugUi.GetComponent<TextMeshPro>();
-			DebugUi.SetActive(isDebugMode.Value);
-			return DebugUi;
-		}
-
-		public static void PrintInGame(string message)
-		{
-			if(!(DebugUi is null))
-			{
-				DebugUiText = message;
-			}
-			else 
-			{
-				Log($"Can't print message: \"{message}\" to debug ui. Not created and assigned", false, 2);
-			}
-		}
-
-		public static GameObject DebugUi {get; private set;}
-		public static TextMeshPro DebugUiText {get; private set;}
-		public static GameObject CreateDebugUi(GameObject PlayerUi)
-		{
-			DebugUi = Calls.Create.NewText("Placeholder text. You shouldn't be seeing this without some UE Shenanigans\n or decompiled code. Doesn't count if it's you, Ava. I (probably) told you about this.", 1f, Color.white, new Vector3(0f, 0.1f, 1f), Quaternion.Euler(0, 0, 0));
-			DebugUi.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-			DebugUi.transform.localPosition = new Vector3(0f, 0.1f, 0.96f);
-			DebugUi.transform.SetParent(PlayerUi.transform, false);
-			DebugUiText = DebugUi.GetComponent<TextMeshPro>();
 			DebugUi.SetActive(debugMode);
 			PrintInGame("");
 			return DebugUi;
