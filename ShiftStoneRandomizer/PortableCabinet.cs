e2cb8cea93d39c317ca163672ce75d4780034d8c
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2025-12-14 22:35:41 -0500

    Created new initial file for portable cabinet

---
+++ ShiftStoneRandomizer/PortableCabinet.cs
@@ -0,0 +1,22 @@
+using Il2CppTMPro;
+using MelonLoader;
+using Il2CppRUMBLE.Combat.ShiftStones;
+using Il2CppRUMBLE.Interactions.InteractionBase;
+using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
+using RumbleModdingAPI;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Il2CppRUMBLE.Managers;
+using UnityEngine.Bindings;
+
+namespace ShiftStoneRandomizer
+{
+	public class PortableCabinet
+    {
+        private GameObject Cubbies {get; private set;}
+        public GameObject StoneCase {get; private set; }
+        
+    }
+}
