d37f3f18e0bf6635e2c7c17f55ad1cf49f1989e5
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2026-05-26 08:04:03 -0400

    Added prefab source variables.

---
+++ ShiftStoneRandomizer/src/PrefabManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using RumbleModdingAPI;
+using RumbleModdingAPI.RMAPI;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.UI;
+namespace ShiftStoneManager.src
+{
+    class PrefabManager
+    {
+        internal static GameObject BigButtonSource;
+        internal static GameObject SmallButtonSource;
+
+        internal static GameObject CubbyButtonSource;
+
+        internal static GameObject[] ShiftStoneSource = new GameObject[8];
+
+
+    }
+}
